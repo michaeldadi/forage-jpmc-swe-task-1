0cf086dd611bf3bed89916e4ce1deba0bf01a61a
Michael Dadi <13662053+michaeldadi@users.noreply.github.com>
2023-03-31 04:41:13 -0400

    Added unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 
 class ClientTest(unittest.TestCase):
@@ -11,6 +11,9 @@ class ClientTest(unittest.TestCase):
              'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
         ]
         """ ------------ Add the assertion below ------------ """
+        for quote in quotes:
+            self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],
+                                                   (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
     def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
         quotes = [
@@ -20,9 +23,19 @@ class ClientTest(unittest.TestCase):
              'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
         ]
         """ ------------ Add the assertion below ------------ """
+        for quote in quotes:
+            self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],
+                                                   (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
     """ ------------ Add more unit tests ------------ """
+    def test_getRatio(self):
+        prices = [127.06, 128.23, 133.08, 129.54, 137.31, 135.36, 136.57, 127.645]
+        for i in range(0, len(prices), 2):
+            self.assertEqual(getRatio(prices[i], prices[i + 1]), prices[i] / prices[i + 1])
+        self.assertEqual(getRatio(1, 2), 0.5)
 
+    def test_getRatio_zeroDivision(self):
+        self.assertIsNone(getRatio(1, 0))
 
 if __name__ == '__main__':
     unittest.main()
